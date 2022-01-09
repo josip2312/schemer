69c752dc630246883bc94198f0cc6b7cb2f4e4fd
Josip Ivancic <ivancic.josip@nsoft.com>
2022-01-09 14:51:54 +0100

    Add config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), Components({ dts: true })],
   resolve: {
     alias: {
-      '@/': new URL('./src/', import.meta.url).pathname
-    }
-  }
-})
+      '@/': new URL('./src/', import.meta.url).pathname,
+    },
+  },
+});
